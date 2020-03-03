26ccefae6d3ba45d74c98b06dfae2be637a219c6
github username <github email address>
2020-03-03 09:31:57 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   resources :bookings
+
+  root to: 'bookings#index'
+  get '/bookings/new', to: 'bookings#new', as: 'bookings'
+  post '/bookings', to: 'bookings#create'
+
+  get '/admin', to: 'admin#login', as: 'admin_login'
+  post '/admin', to: 'admin#confirmed'
+  get '/admin/bookings', to: 'admin#confirm_booking'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
